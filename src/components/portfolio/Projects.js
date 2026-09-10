8fc09d9364964c258f6c3b365d6bd16ceacd5e5f
Ali Raxa Khan <alikhan.cse15@gmail.com>
2026-09-10 06:14:57 +0500

    Completed Vercel serverless API

--- src/components/portfolio/Projects.js
+++ src/components/portfolio/Projects.js
@@ -14,7 +14,7 @@ const Projects = () => {
 
   const fetchProjects = async () => {
     try {
-      const response = await fetch('http://localhost:5000/api/projects');
+     const response = await fetch('/api/projects');
       const data = await response.json();
       
       if (data.success) {
