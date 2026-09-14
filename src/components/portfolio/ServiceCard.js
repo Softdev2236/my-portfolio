c66cf656e5b883d660dea99643e8609bd0de05f9
Ali Raxa Khan <alikhan.cse15@gmail.com>
2026-09-14 15:44:07 +0500

    Fixed ServiceCard to map icon strings to components

--- src/components/portfolio/ServiceCard.js
+++ src/components/portfolio/ServiceCard.js
@@ -1,17 +1,45 @@
+import {
+  FaReact,
+  FaPaintBrush,
+  FaVideo,
+  FaRobot,
+  FaFileExcel,
+  FaCode,
+  FaMobile,
+  FaPalette,
+  FaCamera,
+  FaChartLine
+} from 'react-icons/fa';
+
+// Map icon names (strings from MongoDB) to actual React components
+const iconMap = {
+  FaReact: <FaReact />,
+  FaPaintBrush: <FaPaintBrush />,
+  FaVideo: <FaVideo />,
+  FaRobot: <FaRobot />,
+  FaFileExcel: <FaFileExcel />,
+  FaCode: <FaCode />,
+  FaMobile: <FaMobile />,
+  FaPalette: <FaPalette />,
+  FaCamera: <FaCamera />,
+  FaChartLine: <FaChartLine />,
+  // Default fallback
+  default: <FaCode />
+};
+
 const ServiceCard = ({ service }) => {
-    return (
-        <div className="service-card">
+  // Get the icon component from the map, or use fallback
+  const iconComponent = iconMap[service.icon] || iconMap.default;
 
-            <div className="service-icon">
-                {service.icon}
-            </div>
-
-            <h3>{service.title}</h3>
-
-            <p>{service.description}</p>
-
-        </div>
-    );
+  return (
+    <div className="service-card">
+      <div className="service-icon">
+        {iconComponent}
+      </div>
+      <h3>{service.title}</h3>
+      <p>{service.description}</p>
+    </div>
+  );
 };
 
 export default ServiceCard;
